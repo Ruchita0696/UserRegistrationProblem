96ea17972985997ac83abdf6ceb42eb81022c06b
ruchi06 <ruchi06@gmail.com>
2023-01-24 12:48:33 +0530

    Last Name

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -5,9 +5,7 @@
         static void Main(string[] args)
         {
             UserRegistation user = new UserRegistation();
-            Console.Write("Enter First Name : ");
-            string firstName = Console.ReadLine();
-            user.FirstNameAndLastName(firstName);
+            user.UserInputName();
         }
     }
 }
--- UserRegistrationProblem/UserRegistation.cs
+++ UserRegistrationProblem/UserRegistation.cs
@@ -9,18 +9,36 @@ namespace UserRegistrationProblem
 {
     internal class UserRegistation
     {
+        bool pass = false;
+        public void UserInputName()
+        {
+            if (pass == false)
+            {
+                Console.Write("Enter First Name : ");
+                string firstName = Console.ReadLine();
+                FirstNameAndLastName(firstName);
+            }
+            if (pass == true)
+            {
+                Console.Write("Enter Last Name : ");
+                string LastName = Console.ReadLine();
+                FirstNameAndLastName(LastName);
+            }
+        }
         public void FirstNameAndLastName(string Name)
         {
             Regex firstLastName = new Regex(@"^([A-z]{1}[a-z]{2,})$");
             bool matches = firstLastName.IsMatch(Name);
-
             if (matches == true)
             {
+                pass = true;
                 Console.WriteLine($"{Name} is Valid Name");
+                return;
             }
             else
             {
                 Console.WriteLine($"Please Enter Valid Name {Name}");
+                UserInputName();
             }
         }
     }
